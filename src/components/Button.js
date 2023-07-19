5a5afe3d026f7cf19f584878257809ff455ccb05
Sethu Nair <sethunair2102@gmail.com>
2023-07-19 11:10:07 -0600

    Daylist and DayListItem components, tests and styles completed

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,27 +3,18 @@ import classNames from "classnames";
 import "components/Button.scss";
 
 export default function Button(props) {
-const buttonClass = classNames ("button",{
-   "button--confirm":props.confirm,
-   "button--danger": props.danger
-});
-
-if (props.confirm){
-   buttonClass+= " button--confirm";
-} 
-
-if (props.danger){
-   buttonClass+= " button--danger";
-} 
-
+   const buttonClass = classNames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+   });
 
    return (
-   <button 
-   className= {buttonClass}
-   onClick={props.onClick}
-   disabled={props.disabled}
-   > 
-   {props.children} 
-   </button>
+      <button
+         className={buttonClass}
+         onClick={props.onClick}
+         disabled={props.disabled}
+      >
+         {props.children}
+      </button>
    );
 }
