eb1a78a30a7df49d2f9a65b20ba69fcfdd76c1e4
Sethu Nair <sethunair2102@gmail.com>
2023-07-22 10:24:08 -0600

    Interviewer List components added and refactored

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "components/InterviewerListItem.scss"
+import classNames from "classnames";
+
+export default function InterviewerListItem(props) {
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected
+  })
+
+  return (
+    <li 
+    onClick={props.setInterviewer}
+    className={interviewerClass}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && <p> {props.name} </p >}
+    </li>
+  )   
+}
