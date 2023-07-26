fd3ea608695d287a75c6e999408379e1d2bfde50
Sethu Nair <sethunair2102@gmail.com>
2023-07-26 00:54:34 -0600

    Appointment complete

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -5,13 +5,13 @@ export default function DayList(props) {
 
   const dayArray = props.days.map((day) => {
     return (
-        <DayListItem
-          key={day.id}
-          name={day.name}
-          spots={day.spots}
-          selected={day.name === props.day}
-          setDay={props.setDay} />
-    )
+      <DayListItem
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.value}
+        setDay={props.onChange} />
+  )
   })
 
   return (
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -13,7 +13,9 @@ export default function DayListItem(props) {
   return (
     <li
       className={listClass}
-      onClick={() => props.setDay(props.name)}>
+      onClick={() => props.setDay(props.name)} 
+      selected={props.selected}
+      >
       <h2 className="text--regular"> {props.name}</h2>
       <h3 className="text--light"> {formatSpots(props.spots)}</h3>
     </li>
